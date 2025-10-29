1f64e71ff8e6c8b2fd50f91df372028f6cbd02a4
saneatsu <w.saneatsu@gmail.com>
2025-10-29 20:10:19 +0900

    style: Update footer padding on mobile

--- apps/web/src/widgets/footer/ui/footer/footer.tsx
+++ apps/web/src/widgets/footer/ui/footer/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
 	const t = useTranslations("footer");
 
 	return (
-		<footer className="border-t pb-20 md:pb-0">
+		<footer className="border-t pb-12 md:pb-0">
 			<div className="container mx-auto px-4">
 				<div className="max-w-7xl mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
 					<div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
