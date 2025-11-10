6f5aaea564f11fa634ad5d4b82ae316a23586013
saneatsu <w.saneatsu@gmail.com>
2025-11-11 00:55:50 +0900

    test: Add contribution aggregator specs

---
+++ packages/db/src/lib/contributions/contribution-aggregator.ts
@@ -0,0 +1,17 @@
+/**
+ * NOTE: 実装はこれから行う。まずはテストを先行して用意するためのスタブ。
+ */
+export function countJapaneseCharactersFromMarkdown(_markdown: string): number {
+	throw new Error("Not implemented yet");
+}
+
+export function calculateJapaneseCharDelta(
+	_prevContent: string | null,
+	_nextContent: string
+): number {
+	throw new Error("Not implemented yet");
+}
+
+export function toJstDateKey(_date: Date): string {
+	throw new Error("Not implemented yet");
+}
