c08f7e0ed96965c4565ced06bc491e1f1d37ee03
saneatsu <w.saneatsu@gmail.com>
2025-11-11 14:59:55 +0900

    chore(backend): Fromat public route handler

--- apps/backend/src/routes/public/index.ts
+++ apps/backend/src/routes/public/index.ts
@@ -5,5 +5,7 @@ import type { Env } from "@/env";
 import { getPublicContributions } from "./handlers/get-contributions/get-contributions";
 import { getPublicContributionsRoute } from "./handlers/get-contributions/get-contributions.openapi";
 
-export const publicRoute = new OpenAPIHono<{ Bindings: Env }>()
-	.openapi(getPublicContributionsRoute, getPublicContributions);
+export const publicRoute = new OpenAPIHono<{ Bindings: Env }>().openapi(
+	getPublicContributionsRoute,
+	getPublicContributions
+);
